0575adacd44c85e9314d92189b92f6101edcf3c8
Adrien Antoine <adriantoine@gmail.com>
2016-12-07 11:58:56 +0000

    fix: Additional check to avoid potential bug

--- src/utils.js
+++ src/utils.js
@@ -6,6 +6,6 @@ const REACT_WRAPPER_NAME = ReactWrapper.name;
 
 export const isShallowWrapper = wrapper => wrapper && wrapper.constructor && wrapper.constructor.name === SHALLOW_WRAPPER_NAME;
 export const isReactWrapper = wrapper => wrapper && wrapper.constructor && wrapper.constructor.name === REACT_WRAPPER_NAME;
-export const isCheerioWrapper = wrapper => wrapper.cheerio;
+export const isCheerioWrapper = wrapper => wrapper && wrapper.cheerio;
 
 export const isEnzymeWrapper = wrapper => isShallowWrapper(wrapper) || isReactWrapper(wrapper) || isCheerioWrapper(wrapper);
