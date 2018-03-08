b12686ade62593c873c61040bf5aac82227eb426
Michael Dougall <madou@users.noreply.github.com>
2018-03-08 21:25:50 +1100

    bugfix(default-props): excludes default props from snapshops in shallow mode (#91)

--- tests/shallow.test.js
+++ tests/shallow.test.js
@@ -14,12 +14,15 @@ import {
   FalsyChildren,
   FragmentAsChild,
   FragmentAsRoot,
+  WithDefaultProps,
+  ComponentWithChildren,
 } from './fixtures/pure-function';
 import {
   BasicClass,
   ClassWithPure,
   ClassWithNull,
   ClassArrayRender,
+  ClassWithDefaultProps,
 } from './fixtures/class';
 
 Enzyme.configure({adapter: new Adapter()});
@@ -122,7 +125,9 @@ it('ignores non-plain objects', () => {
     this._test = true;
   }
 
-  const shallowed = shallow(<WrapperComponent instance={new TestConstructor()} />);
+  const shallowed = shallow(
+    <WrapperComponent instance={new TestConstructor()} />,
+  );
 
   expect(shallowToJson(shallowed)).toMatchSnapshot();
 });
@@ -263,6 +268,46 @@ it('renders a component that has a child fragment', () => {
 
 it('renders a component that has a fragment root', () => {
   const wrapper = shallow(<FragmentAsRoot />);
+
+  expect(shallowToJson(wrapper)).toMatchSnapshot();
+});
+
+it('should not bleed default props from funtional child component into snapshot', () => {
+  const wrapper = shallow(
+    <ComponentWithChildren>
+      <WithDefaultProps />
+    </ComponentWithChildren>,
+  );
+
+  expect(shallowToJson(wrapper)).toMatchSnapshot();
+});
+
+it('should set prop that has a different value from default prop values of functional component', () => {
+  const wrapper = shallow(
+    <ComponentWithChildren>
+      <WithDefaultProps value="yeah, man" />
+    </ComponentWithChildren>,
+  );
+
+  expect(shallowToJson(wrapper)).toMatchSnapshot();
+});
+
+it('should not bleed default props from class child component into snapshot', () => {
+  const wrapper = shallow(
+    <ComponentWithChildren>
+      <ClassWithDefaultProps />
+    </ComponentWithChildren>,
+  );
+
+  expect(shallowToJson(wrapper)).toMatchSnapshot();
+});
+
+it('should set prop that has a different value from default prop values of class component', () => {
+  const wrapper = shallow(
+    <ComponentWithChildren>
+      <ClassWithDefaultProps value="ah, man" />
+    </ComponentWithChildren>,
+  );
 
   expect(shallowToJson(wrapper)).toMatchSnapshot();
 });
