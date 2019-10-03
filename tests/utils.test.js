581a6128ce3ad9ca81401254f434ef1d04ca5f28
Vincent Langlet <VincentLanglet@users.noreply.github.com>
2019-10-03 12:14:27 +0200

    Revert "Fix check of node type "

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -1,15 +1,11 @@
 /* eslint-env jest */
 
-// import React from 'react';
-import React, {memo, lazy, Suspense} from 'react';
+import React from 'react';
 import Enzyme, {shallow, mount, render} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import {isEnzymeWrapper, extractTypeName} from '../src/utils';
+import {isEnzymeWrapper} from '../src/utils';
 
 Enzyme.configure({adapter: new Adapter()});
-
-const Lazy = lazy(() => Promise.resolve(<div>lazy div</div>));
-const Memo = memo(() => <div>memoized div</div>);
 
 it('returns true whenever it is an enzyme wrapper', () => {
   expect(isEnzymeWrapper(shallow(<div />))).toBe(true);
@@ -22,9 +18,3 @@ it('returns false whenever it is an enzyme wrapper', () => {
   expect(isEnzymeWrapper('test')).toBe(false);
   expect(isEnzymeWrapper(<div />)).toBe(false);
 });
-
-it('returns correct node type', () => {
-  expect(extractTypeName(<Lazy />)).toBe('React.Lazy');
-  expect(extractTypeName(<Memo />)).toBe('React.Memo');
-  expect(extractTypeName(<Suspense />)).toBe('React.Suspense');
-})
